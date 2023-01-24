abedfce0a2e9e41870eaf7a9ba9d6291898626cd
LlamaUnicorn <holidaynight@gmail.com>
2023-01-24 06:52:08 +0300

    Drafted models 2 and 3 fields

--- silant/service_portal/models.py
+++ silant/service_portal/models.py
@@ -3,31 +3,51 @@ from django.db import models
 # Create your models here.
 
 
-class Vehicles(models.Model):  # наследуемся от класса Model
+class Vehicle(models.Model):  # наследуемся от класса Model
     # vehicle_serial_number = models.CharField(max_length = 255)
     # vehicle_model = models.CharField(max_length = 255)
-    # engine_model = models.CharField(max_length = 255)
-    # engine_serial_number = models.CharField(max_length = 255)
-    # transmission_model = models.CharField(max_length = 255)
-    # transmission_serial_number = models.CharField(max_length = 255)
-    # live_axle_model = models.CharField(max_length = 255)
-    # live_axle_serial_number = models.CharField(max_length = 255)
-    # dead_axle_model = models.CharField(max_length = 255)
-    # dead_axle_serial_number = models.CharField(max_length = 255)
-    # invoice = models.CharField(max_length = 255)
-    # shipping_date = models.CharField(max_length = 255)
-    # end_user = models.CharField(max_length = 255)
-    # shipping_address = models.CharField(max_length = 255)
-    # options = models.CharField(max_length = 255)
-    # client = models.CharField(max_length = 255)
+    # vehicle_engine_model = models.CharField(max_length = 255)
+    # vehicle_engine_serial_number = models.CharField(max_length = 255)
+    # vehicle_transmission_model = models.CharField(max_length = 255)
+    # vehicle_transmission_serial_number = models.CharField(max_length = 255)
+    # vehicle_live_axle_model = models.CharField(max_length = 255)
+    # vehicle_live_axle_serial_number = models.CharField(max_length = 255)
+    # vehicle_dead_axle_model = models.CharField(max_length = 255)
+    # vehicle_dead_axle_serial_number = models.CharField(max_length = 255)
+    # vehicle_invoice = models.CharField(max_length = 255)
+    # vehicle_shipping_date = models.CharField(max_length = 255)
+    # vehicle_end_user = models.CharField(max_length = 255)
+    # vehicle_shipping_address = models.CharField(max_length = 255)
+    # vehicle_options = models.CharField(max_length = 255)
+    # vehicle_client = models.CharField(max_length = 255)
+    # vehicle_service_provider = models.CharField(max_length = 255)
+
+    pass
+
+
+class Service(models.Model):  # наследуемся от класса Model
+    # service_type = models.CharField(max_length = 255)
+    # service_date = models.CharField(max_length = 255)
+    # service_engine_hours = models.CharField(max_length = 255)
+    # service_order_number = models.CharField(max_length = 255)
+    # service_order_date = models.CharField(max_length = 255)
+    # service_organization = models.CharField(max_length = 255)
+    # service_vehicle = models.CharField(max_length = 255)
     # service_provider = models.CharField(max_length = 255)
 
     pass
 
 
-class Services(models.Model):  # наследуемся от класса Model
-    pass
+class Reclamation(models.Model):  # наследуемся от класса Model
+    # reclamation_date= models.CharField(max_length = 255)
+    # reclamation_engine_hours= models.CharField(max_length = 255)
+    # reclamation_malfunction= models.CharField(max_length = 255)
+    # reclamation_malfunction_description= models.CharField(max_length = 255)
+    # reclamation_repair_type= models.CharField(max_length = 255)
+    # reclamation_placement_parts= models.CharField(max_length = 255)
+    # reclamation_repair_date= models.CharField(max_length = 255)
+    # reclamation_idle_time= models.CharField(max_length = 255)
+    # reclamation_vehicle= models.CharField(max_length = 255)
+    # reclamation_service_provider= models.CharField(max_length = 255)
 
-
-class Reclamations(models.Model):  # наследуемся от класса Model
     pass
