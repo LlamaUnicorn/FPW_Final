2e51c93cfda53cc800360772f47c659cd75c3aef
LlamaUnicorn <holidaynight@gmail.com>
2023-02-08 06:49:26 +0300

    Added 3 more linked pages

--- silant/service_portal/urls.py
+++ silant/service_portal/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index, VehicleCreateView, directory_vehicle_model_view
+from .views import *
 
 urlpatterns = [
     path('create/', VehicleCreateView.as_view(), name='create'),
     path('', index, name='index'),
     path('vehicle_model/<int:pk>', directory_vehicle_model_view, name='directory_vehicle_model_view'),
+    path('vehicle_engine_model/<int:pk>', directory_vehicle_engine_model_view, name='directory_vehicle_engine_model_view'),
+    path('vehicle_transmission_model/<int:pk>', directory_vehicle_transmission_model_view, name='directory_vehicle_transmission_model_view'),
+    path('vehicle_live_axle_model/<int:pk>', directory_vehicle_live_axle_model_view,
+         name='directory_vehicle_live_axle_model_view'),
+
 ]
--- silant/service_portal/views.py
+++ silant/service_portal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-from .models import Vehicle, DirectoryVehicleModel
+from .models import *
 from .forms import VehicleForm
 
 
@@ -17,6 +17,21 @@ def directory_vehicle_model_view(request, pk):
 # def directory_vehicle_model_view(request):
 #     vehicle_models = DirectoryVehicleModel.objects.all
 #     return render(request, 'service_portal/DirectoryVehicleModel.html', {'vehicle_models': vehicle_models})
+
+
+def directory_vehicle_engine_model_view(request, pk):
+    vehicle_engine_models = DirectoryVehicleEngineModel.objects.filter(pk=pk)
+    return render(request, 'service_portal/DirectoryVehicleEngineModel.html', {'vehicle_engine_models': vehicle_engine_models})
+
+
+def directory_vehicle_transmission_model_view(request, pk):
+    vehicle_transmission_models = DirectoryVehicleTransmissionModel.objects.filter(pk=pk)
+    return render(request, 'service_portal/DirectoryVehicleTransmissionModel.html', {'vehicle_transmission_models': vehicle_transmission_models})
+
+
+def directory_vehicle_live_axle_model_view(request, pk):
+    vehicle_live_axle_models = DirectoryVehicleLiveAxleModel.objects.filter(pk=pk)
+    return render(request, 'service_portal/DirectoryVehicleLiveAxleModel.html', {'vehicle_live_axle_models': vehicle_live_axle_models})
 
 
 class VehicleCreateView(CreateView):
