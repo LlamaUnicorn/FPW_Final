eb554a336a35665dc29a0e6160dd1b6c788cf819
LlamaUnicorn <holidaynight@gmail.com>
2023-02-09 07:01:22 +0300

    'custom_filters' is not a registered tag library. Register the tag later

--- silant/service_portal/filters.py
+++ silant/service_portal/filters.py
@@ -1,11 +1,9 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter
 from .models import Vehicle
 
 
 class VehicleFilter(FilterSet):
+    vehicle_model = CharFilter(field_name='vehicle_model__directory_vehicle_model', lookup_expr='icontains')
     class Meta:
         model = Vehicle
-        fields = {
-            'vehicle_model' = ['icontains'],
-            'vehicle_engine_model' = ['icontains'],
-        }
+        fields = ['vehicle_model']
--- silant/service_portal/views.py
+++ silant/service_portal/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 
 from .models import *
 from .forms import VehicleForm
+from .filters import VehicleFilter
 
 
 def index(request):
@@ -14,6 +15,8 @@ def index(request):
 def directory_vehicle_model_view(request, pk):
     vehicle_models = DirectoryVehicleModel.objects.filter(pk=pk)
     return render(request, 'service_portal/DirectoryVehicleModel.html', {'vehicle_models': vehicle_models})
+
+
 # def directory_vehicle_model_view(request):
 #     vehicle_models = DirectoryVehicleModel.objects.all
 #     return render(request, 'service_portal/DirectoryVehicleModel.html', {'vehicle_models': vehicle_models})
@@ -21,32 +24,38 @@ def directory_vehicle_model_view(request, pk):
 
 def directory_vehicle_engine_model_view(request, pk):
     vehicle_engine_models = DirectoryVehicleEngineModel.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryVehicleEngineModel.html', {'vehicle_engine_models': vehicle_engine_models})
+    return render(request, 'service_portal/DirectoryVehicleEngineModel.html',
+                  {'vehicle_engine_models': vehicle_engine_models})
 
 
 def directory_vehicle_transmission_model_view(request, pk):
     vehicle_transmission_models = DirectoryVehicleTransmissionModel.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryVehicleTransmissionModel.html', {'vehicle_transmission_models': vehicle_transmission_models})
+    return render(request, 'service_portal/DirectoryVehicleTransmissionModel.html',
+                  {'vehicle_transmission_models': vehicle_transmission_models})
 
 
 def directory_vehicle_live_axle_model_view(request, pk):
     vehicle_live_axle_models = DirectoryVehicleLiveAxleModel.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryVehicleLiveAxleModel.html', {'vehicle_live_axle_models': vehicle_live_axle_models})
+    return render(request, 'service_portal/DirectoryVehicleLiveAxleModel.html',
+                  {'vehicle_live_axle_models': vehicle_live_axle_models})
 
 
 def directory_vehicle_dead_axle_model_view(request, pk):
     vehicle_dead_axle_models = DirectoryVehicleDeadAxleModel.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryVehicleDeadAxleModel.html', {'vehicle_dead_axle_models': vehicle_dead_axle_models})
+    return render(request, 'service_portal/DirectoryVehicleDeadAxleModel.html',
+                  {'vehicle_dead_axle_models': vehicle_dead_axle_models})
 
 
 def directory_clients_model_view(request, pk):
     vehicle_clients_models = DirectoryClientsModel.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryClientsModel.html', {'vehicle_clients_models': vehicle_clients_models})
+    return render(request, 'service_portal/DirectoryClientsModel.html',
+                  {'vehicle_clients_models': vehicle_clients_models})
 
 
 def directory_vehicle_service_provider_view(request, pk):
     vehicle_service_providers = DirectoryServiceProvider.objects.filter(pk=pk)
-    return render(request, 'service_portal/DirectoryServiceProvider.html', {'vehicle_service_providers': vehicle_service_providers})
+    return render(request, 'service_portal/DirectoryServiceProvider.html',
+                  {'vehicle_service_providers': vehicle_service_providers})
 
 
 class VehicleCreateView(CreateView):
@@ -57,3 +66,26 @@ class VehicleCreateView(CreateView):
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     pass
+
+
+class VehicleList(ListView):
+    model = Vehicle
+    ordering = 'vehicle_shipping_date'
+    template_name = 'index.html'
+    context_object_name = 'vehicles'
+
+    def get_queryset(self):
+        queryset = super.get_queryset()
+        self.filterset = VehicleFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class VehicleDetail(DetailView):
+    model = Vehicle
+    template_name = 'DirectoryVehicleModel.html'
+    context_object_name = 'vehicle'
