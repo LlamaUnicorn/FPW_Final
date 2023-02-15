1ef38a6044ebbcf0889c532ddb7b6c3f2f5f1261
LlamaUnicorn <holidaynight@gmail.com>
2023-02-15 12:44:01 +0300

    Cleaned up urls.py

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 # Account info:
 admin:admin
+
 test_service:123qwezxc
+
 test_manager:123qwezxc
+
--- silant/service_portal/urls.py
+++ silant/service_portal/urls.py
@@ -6,9 +6,14 @@ from . import views
 from service_portal.views import VehicleDetailView, VehicleManagersDetailView, VehicleServiceDetailView
 
 urlpatterns = [
-    path('create/', VehicleCreateView.as_view(), name='create'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('', index, name='index'),
     path('auth_index/', authorized_index_view, name='auth_index'),
+    path('create/', VehicleCreateView.as_view(), name='create'),
+    path('vehicle/<int:pk>/', VehicleDetailView.as_view(), name='vehicle-detail'),
+    path('vehicle/<int:pk>/update/', VehicleUpdateView.as_view(), name='update'),
+    path('vehicle/<int:pk>/delete/', VehicleDeleteView.as_view(), name='delete'),
     path('vehicle_serial_number/<int:pk>', directory_vehicle_model_view, name='directory_vehicle_model_view'),
     path('vehicle_engine_model/<int:pk>', directory_vehicle_engine_model_view, name='directory_vehicle_engine_model_view'),
     path('vehicle_transmission_model/<int:pk>', directory_vehicle_transmission_model_view, name='directory_vehicle_transmission_model_view'),
@@ -16,13 +21,8 @@ urlpatterns = [
     path('vehicle_dead_axle_model/<int:pk>', directory_vehicle_dead_axle_model_view, name='directory_vehicle_dead_axle_model_view'),
     path('vehicle_clients_model/<int:pk>', directory_clients_model_view, name='directory_clients_model_view'),
     path('vehicle_service_provider/<int:pk>', directory_vehicle_service_provider_view, name='directory_vehicle_service_provider_view'),
-    path('vehicle/<int:pk>/', VehicleDetailView.as_view(), name='vehicle-detail'),
     path('vehicle-managers/<int:pk>/', VehicleManagersDetailView.as_view(), name='vehicle-managers-detail'),
     path('vehicle-service/<int:pk>/', VehicleServiceDetailView.as_view(), name='vehicle-service-detail'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('logout/', views.logout_view, name='logout'),
-    # path('auth_index/', AuthorizedIndexView.as_view(), name='auth_index')
     path('service_view/', views.service_view, name='service_view'),
     path('reclamation_view/', views.reclamation_view, name='reclamation_view'),
     path('vehicle_view/', views.vehicle_view, name='vehicle_view'),
@@ -32,6 +32,4 @@ urlpatterns = [
          name='directory_reclamation_repair_type_view'),
     path('service_service_type/<int:pk>', directory_service_types_view,
          name='directory_service_types_view'),
-    path('vehicle/<int:pk>/update/', VehicleUpdateView.as_view(), name='update'),
-    path('vehicle/<int:pk>/delete/', VehicleDeleteView.as_view(), name='delete'),
 ]
